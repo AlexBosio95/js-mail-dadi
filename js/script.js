9b4f4ca9ae845665f8cd6d4fdbf8f27627118948
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-17 16:15:19 +0200

    email comparison

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const elencoMail = ['pippo@gmail.com', 'pluto@gmail.com', 'giacomo@gmail.com', 'pino@gmail.com', 'umberto@gmail.com', 'peppino@gmail.com', 'sara@gmail.com', 'linda@gmail.com', 'francesca@gmail.com', 'serena@gmail.com', 'monica@gmail.com', 'alessandro@gmail.com', 'luca@gmail.com', 'rebecca@gmail.com', 'dino@gmail.com'];
+
+const emailUser = prompt('Inserisci la tua email');
+
+let verify = false;
+
+for (let index = 0; index < elencoMail.length; index++) {
+    
+    if (emailUser == elencoMail[index]){
+        verify = true;
+    } else{
+    }
+}
+
+console.log(verify);
+
+if (verify == true){
+    console.log('Puoi entrare')
+} else {
+    console.log('Accesso negato l\'email non è presente')
+}
