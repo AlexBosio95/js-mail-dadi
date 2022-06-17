854c35043c0d68be6089ce1d0fb01ae5098e9cad
AlexBosio95 <alex.bosio1995@gmail.com>
2022-06-17 17:35:00 +0200

    fix random numbers

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
+// Array con elenco email
 const elencoMail = ['pippo@gmail.com', 'pluto@gmail.com', 'giacomo@gmail.com', 'pino@gmail.com', 'umberto@gmail.com', 'peppino@gmail.com', 'sara@gmail.com', 'linda@gmail.com', 'francesca@gmail.com', 'serena@gmail.com', 'monica@gmail.com', 'alessandro@gmail.com', 'luca@gmail.com', 'rebecca@gmail.com', 'dino@gmail.com'];
+// variabile dove viene scritta l'email
+const emailUser = document.getElementById('basic-addon1');
+// Variabile dove scrivo il risultato di accesso
+const checkUser = document.getElementById('check-user');
+// Variabile bottone di controllo
+const verEmail = document.getElementById('btn-check');
 
-const emailUser = prompt('Inserisci la tua email');
-
+// Verifica il vero o falso 
 let verify = false;
 
-for (let index = 0; index < elencoMail.length; index++) {
+// Evento click per contollo email
+verEmail.addEventListener('click', function(){
 
-    if (emailUser == elencoMail[index]) {
-        verify = true;
+    for (let index = 0; index < elencoMail.length; index++) {
+
+        if (emailUser.value == elencoMail[index]) {
+            verify = true;
+        }
     }
-}
 
-if (verify == true) {
-    console.log('Puoi entrare')
-} else {
-    console.log('Accesso negato l\'email non è presente')
-}
+    if (verify == true) {
+        console.log('Puoi entrare');
+        checkUser.innerHTML = ('Puoi entrare');
+        
+    } else {
+        console.log('Accesso negato l\'email non è presente');
+        checkUser.innerHTML = ('Accesso negato l\'email non è presente');
+    }
+
+    console.log(emailUser.value);
+    
+});
+
+
+
+
 
 
 // Gioco dei dadi 
 
-const userRandom = Math.floor(Math.random() * 6);
-const pcRandom = Math.floor(Math.random() * 6);
+const userRandom = Math.floor((Math.random() * 6) + 1);
+const pcRandom = Math.floor((Math.random() * 6) + 1);
 
 console.log('User' + ' ' + userRandom);
 console.log('Pc' + ' ' + pcRandom);
